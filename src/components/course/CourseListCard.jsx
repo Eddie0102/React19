4332245e07bb8f68745cee1dc0b1f78dcf819430
Eddie <youngje0102@naver.com>
2025-03-16 16:13:43 +0900

    JSX
    How to wirte JSX in React Component

--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -2,7 +2,7 @@ import CourseItem from './CourseItem';
 
 function CourseListCard(props) {
     return (
-        <div className="card">
+        <div className="card" style={{ backgroundColor: 'lightgrey' }}>
             <div className="card__header">강의 목록</div>
             <div className="card__body">
                 <div className="courses">
